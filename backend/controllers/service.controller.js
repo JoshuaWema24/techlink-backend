77a13e5fdaff85c3ebbe91b227746854bd809812
JoshuaWema24 <wotiajoshua@gmail.com>
2025-08-04 03:20:55 -0700

    Fixing bugs in the server

--- backend/controllers/service.controller.js
+++ backend/controllers/service.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express"); 
+const service = require('../models/service.model');
 
 exports.createService = async (req, res) => {
   try {
